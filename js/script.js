71c98c5730e983f088d37c429317c34492aa0da3
Karol Mencel <karol.mencel@gmail.com>
2020-03-17 22:31:46 +0100

    Update clear code

--- js/script.js
+++ js/script.js
@@ -1,59 +1,51 @@
-var argButtonName, argMoveId, argPlayerMove, argComputerMove,
-    computerMove, playerMove, randomNumber, playerInput, computerWins, playerWins;
+let computerWins = 0,
+  playerWins = 0;
 
-buttonRock = document.getElementById('button-rock');
-buttonPaper = document.getElementById('button-paper');
-buttonScissors = document.getElementById('button-scissors');
-computerWins = 0;
-playerWins = 0;
+const buttonRock = document.getElementById('button-rock'),
+  buttonPaper = document.getElementById('button-paper'),
+  buttonScissors = document.getElementById('button-scissors');
 
-// Funkcja przyciskow
+  // Funkcja przyciskow
 function buttonClicked(argButtonName) {
   clearMessages();
-  console.log(argButtonName + ' został kliknięty');
 
-//Przypisanie wyboru
-function getMoveName(argMoveId) {
-  console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
-  if (argMoveId == 1) {
-    return 'kamień';
-  } else if (argMoveId == 2) {
-    return 'papier';
-  } else {
-    return 'nożyce';
+  //Przypisanie wyboru
+  function getMoveName(argMoveId) {
+    if (argMoveId == 1) {
+      return 'kamień';
+    } else if (argMoveId == 2) {
+      return 'papier';
+    } else {
+      return 'nożyce';
+    }
   }
-}
 
-//Porownanie wyborow
-function displayResult(argPlayerMove, argComputerMove) {
-  console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
-  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
-  if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-    printMessage('Wygrywasz!');
-    playerWins++;
-  } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
-    printMessage('Wygrywasz!');
-    playerWins++;
-  } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
-    printMessage('Wygrywasz!');
-    playerWins++;
-  } else if (argPlayerMove == argComputerMove) {
-    printMessage('Remis');
-  } else {
-    printMessage('Przegrywasz :(');
-    computerWins++;
+  //Porownanie wyborow
+  function displayResult(argPlayerMove, argComputerMove) {
+    printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+    if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
+      printMessage('Wygrywasz!');
+      playerWins++;
+    } else if (argPlayerMove == 'kamień' && argComputerMove == 'nożyce') {
+      printMessage('Wygrywasz!');
+      playerWins++;
+    } else if (argPlayerMove == 'nożyce' && argComputerMove == 'papier') {
+      printMessage('Wygrywasz!');
+      playerWins++;
+    } else if (argPlayerMove == argComputerMove) {
+      printMessage('Remis');
+    } else {
+      printMessage('Przegrywasz :(');
+      computerWins++;
+    }
+    printMessage('Wygrane gracza: ' + playerWins + ', wygrane komputera: ' + computerWins);
   }
-  printMessage('Wygrane gracza: ' + playerWins + ', wygrane komputera: ' + computerWins);
-}
 
-console.log('wybór ruchu gracza to: ' + argButtonName);
-playerMove = argButtonName;
-console.log('ruch gracza to: ' + playerMove);
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-computerMove = getMoveName(randomNumber);
-console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
+
+  const playerMove = argButtonName,
+    randomNumber = Math.floor(Math.random() * 3 + 1),
+    computerMove = getMoveName(randomNumber);
+  displayResult(playerMove, computerMove);
 }
 
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
